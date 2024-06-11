9dbe2be894f7b86a11aee03b922f07f58e9274ef
flavieq88 <qinflavie@gmail.com>
2024-06-10 22:26:09 -0400

    part1 reading

--- personal_notes/part1/src/App.jsx
+++ personal_notes/part1/src/App.jsx
@@ -1,8 +1,22 @@
-const App = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world!</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 13
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='George' age={124+23552} />
+      <Hello name='Daisy' age={234} />
+      <Hello name={name} age={age} />
+    </>
   )
 }
 
