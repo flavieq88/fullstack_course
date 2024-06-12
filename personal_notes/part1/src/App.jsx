33a60e01fc3e50c573ece2ecd439c0c6f0bf2aeb
flavieq88 <qinflavie@gmail.com>
2024-06-11 21:23:43 -0400

    part1c

--- personal_notes/part1/src/App.jsx
+++ personal_notes/part1/src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react' // for the state
+
+
 const Hello = (props) => {
-  console.log(props)
   return (
     <div>
       <p>Hello {props.name}, you are {props.age} years old</p>
@@ -7,17 +9,61 @@ const Hello = (props) => {
   )
 }
 
-const App = () => {
+const Display = ({counter}) => { //destructuring props = {counter}
+    return (
+      <div>Counter is at {counter}</div>
+    )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick = {props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = (props) => {
   const name = 'Peter'
   const age = 13
+
+  //for the timer
+  const {counter1} = props
+
+  //for the state component
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseOne = () => setCounter(counter + 1)
+  
+  const setZero = () => setCounter(0)
+
   return (
     <>
       <h1>Greetings</h1>
       <Hello name='George' age={124+23552} />
       <Hello name='Daisy' age={234} />
       <Hello name={name} age={age} />
+      <div>
+        time spent on this webpage = {counter1} seconds
+      </div>
+      <br />
+      <Display counter={counter} />
+      <Button 
+        onClick={increaseOne}
+        text='plus'
+      />
+      <Button 
+        onClick={setZero}
+        text='zero'
+      />
+      <Button 
+        onClick={() => setCounter(counter - 1)} //only one statement so we could just put here directly
+        text='minus'
+      />
     </>
   )
 }
+
+
 
 export default App
--- personal_notes/part1/src/main.jsx
+++ personal_notes/part1/src/main.jsx
@@ -2,5 +2,19 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+let counter1 = 1
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const refresh = () => {
+    root.render(<App counter1={counter1} />)
+}
+
+const intervalID = setInterval( () => {
+    if (counter1 <= 100) {
+        refresh()
+    counter1 += 1
+    } else {
+        clearInterval(intervalID)
+        console.log("spent over 100 seconds")
+    }
+}, 1000)
