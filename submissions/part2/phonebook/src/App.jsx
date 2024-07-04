1e59dcf7d3ec5dca8955b7bb86382a9532fa39ca
Flavie Qin <qinflavie@gmail.com>
2024-07-03 23:21:16 -0400

    non starred part2e

--- submissions/part2/phonebook/src/App.jsx
+++ submissions/part2/phonebook/src/App.jsx
@@ -3,14 +3,16 @@ import { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Notification from './components/Notification';
 
 import personsService from './services/persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState(null);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
+  const [notif, setNotif] = useState({message: null, type: null});
 
   useEffect(() => {
     personsService
@@ -19,6 +21,10 @@ const App = () => {
         setPersons(response.data)
       });
   }, []);
+
+  if (!persons) {
+    return null;
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -31,6 +37,7 @@ const App = () => {
       if (window.confirm(`${newName} is already in the phonebook. Would you like to replace old number with new number?`)) {
         const [double] = persons.filter(person => person.name.toLowerCase() === newName.toLowerCase());
         updateNumber(double, newNumber);
+        console.log("here")
       };
       setNewName('');
       setNewNumber('');
@@ -39,6 +46,12 @@ const App = () => {
       personsService
         .create(nameObject)
         .then(response => setPersons(persons.concat(response.data)));
+
+      setNotif({message: `Added ${nameObject.name}`, type:"good"});
+      setTimeout(() => {
+        setNotif({message: null, type: null})
+      }, 3000);
+
       setNewName('');
       setNewNumber('');
     };
@@ -59,8 +72,19 @@ const App = () => {
   const deletePersonApp = (id) => {
     const person = persons.find(n => n.id === id);
     if (window.confirm(`Delete ${person.name}?`)) {
-      personsService.deletePerson(id);
+      personsService
+        .deletePerson(id)
+        .catch(error => {
+          setNotif({message: `Information of '${person.name}' was already removed from server`, type:"bad"});
+          setTimeout(() => {
+            setNotif({...notif, message: null})
+          }, 3000)
+        });
       setPersons(persons.filter(n => n.id !== id));
+      setNotif({message: `Deleted ${person.name}`, type: "good"});
+      setTimeout(() => {
+        setNotif({...notif, message: null})
+      }, 3000);
     };
   };
 
@@ -68,7 +92,19 @@ const App = () => {
     const newObject = { ...person, number:number}
     personsService
       .update(person.id, newObject)
-      .then(response => setPersons(persons.map(p => p.id !== person.id ? p : response.data)));
+      .then(response => setPersons(persons.map(p => p.id !== person.id ? p : response.data)))
+      .catch(error => {
+        setNotif({message: `Information of '${person.name}' was already removed from server`, type: "bad"});
+        setTimeout(() => {
+          setNotif({message: null, type: null})
+        }, 3000);
+      setPersons(persons.filter(p => p.id !== person.id));
+      });
+
+      setNotif({message: `Changed ${person.name} number`, type:"good"});
+      setTimeout(() => {
+        setNotif({message: null, type: null})
+      }, 3000);
   };
   
 
@@ -76,6 +112,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={notif.message} type={notif.type}/>
 
       <Filter filter={newFilter} handleNewFilter={handleNewFilter}/>
 
