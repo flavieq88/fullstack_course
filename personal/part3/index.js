e8814c03c93f4aebd7f2f1e7c6af39dc6fac2604
Flavie Qin <qinflavie@gmail.com>
2024-07-15 16:28:53 -0400

    frontend backend integration part3c

--- personal/part3/index.js
+++ personal/part3/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const Note = require("./models/note")
 
 app.use(express.static('dist'));
 
 app.use(cors());
 app.use(express.json());
+
+
 
 let notes = [
     {
@@ -39,22 +43,18 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!<h1>');
-});
+
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes);
+    });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(note => note.id === id);
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    };
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -64,35 +64,28 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end();
 });
 
-const generateID = () => {
-    const maxID = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0;
-    return String(maxID + 1);
-};
 
 app.post('/api/notes', (request, response) => {
     const body = request.body;
     
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         });
     };
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateID()
-    };
+        important: body.important || false
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
 }); 
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`)
