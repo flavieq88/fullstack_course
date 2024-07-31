2b58f9a7a43cdbf970a0f25a16df80b8e6ad2ce5
Flavie Qin <qinflavie@gmail.com>
2024-07-31 16:27:16 -0400

    done ex 5.6

--- submissions/part5/src/components/Blog.jsx
+++ submissions/part5/src/components/Blog.jsx
@@ -1,7 +1,53 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [extended, setExtended] = useState(false);
+
+  const closedBlogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    marginBottom: 5
+  };
+
+  const openBlogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    marginBottom: 5,
+    background: 'lightgrey'
+  };
+
+  const toggleExtended = () => {
+    setExtended(!extended);
+  };
+
+  if (!extended) {
+    return (
+      <div style={closedBlogStyle}>
+        {blog.title}, by {blog.author}
+        <button onClick={toggleExtended}>View</button>
+      </div>  
+    );
+  };
+
+  const handleLike = (blog) => {
+    console.log('liking', blog.title);
+  };
+
+  return (
+    <div style={openBlogStyle}>
+      {blog.title}, by {blog.author} <br />
+      {blog.url} <br />
+      {blog.likes} like{blog.likes!==1 && 's'} <button onClick={handleLike(blog)}>Like</button><br />
+      Blog added by user {blog.user.username} <br />
+      <button onClick={toggleExtended}>Hide</button>
+    </div> 
+  )
+};
 
 export default Blog;
